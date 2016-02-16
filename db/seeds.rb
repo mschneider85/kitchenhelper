3e7d57b6bbefd0dadeb4d49842b7001d082eb9b7
mschneider85 <mschneider85@gmx.de>
2016-02-16 17:47:45 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,5 @@
 ['registered', 'banned', 'moderator', 'admin'].each do |role|
   Role.find_or_create_by({name: role})
 end
-User.create(first_name: 'Martin', last_name: 'Schneider', email: 'mschneider85@gmx.de', password: 'changeme')
+u = User.create_with(first_name: 'Martin', last_name: 'Schneider', password: 'changeme').find_or_create_by(email: 'mschneider85@gmx.de')
+u.roles << Role.find_by(name: :admin) unless u.has_role? :admin
